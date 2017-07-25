67e9d7d50de6b9e713acde0f97cc36bff10e158e
Rupashdas <rupash.das.40@gmail.com>
2017-07-25 21:08:14 +0600

    class-fifty-seven

---
+++ class-fifty-seven/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+        
+        $(".author-names").perfectScrollbar({
+            
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	

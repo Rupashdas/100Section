67274e99a0d9a994e3be566cb3ace1a7dbd80511
Rupashdas <rupash.das.40@gmail.com>
2017-08-19 04:12:02 +0600

    seventy-nine

---
+++ class-seventy-nine/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       
+        $(".mailchimp-form").ajaxChimp({
+            url: 'http://Rupashdas.us16.list-manage.com/subscribe/post?u=2dcee63bfed682f0f86b8c86b&amp;id=68ecfe574b'
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	

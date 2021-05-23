bace7ac9c6586f88371a7f406315fa01c30e2494
Rupash Das <rupash.das.40@gmail.com>
2021-05-23 07:18:05 +0600

    Update main.js

--- class-seventy-nine/assets/js/main.js
+++ class-seventy-nine/assets/js/main.js
@@ -6,7 +6,7 @@
 
        
         $(".mailchimp-form").ajaxChimp({
-            url: 'http://Rupashdas.us16.list-manage.com/subscribe/post?u=2dcee63bfed682f0f86b8c86b&amp;id=68ecfe574b'
+            url: 'https://Rupashdas.us16.list-manage.com/subscribe/post?u=2dcee63bfed682f0f86b8c86b&amp;id=68ecfe574b'
         });
 
 
